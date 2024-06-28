4aa982b4ca8e725cb5bb1fe948a1fd6ba8c53215
Majid Ali <majidalipahore55566@gmail.com>
2024-06-28 06:37:08 -0700

    t

--- script.js
+++ script.js
@@ -212,7 +212,7 @@ function displayTopScores(scores) {
     scoreElement.className = "score";
 
     let scoreParagraph = document.createElement("h1");
-    scoreParagraph.innerText = `${score.rollno}: ${score.score}`;
+    scoreParagraph.innerText = `${userId.rollno}: ${scores.score}`;
 
     scoreElement.appendChild(scoreParagraph);
     scoresContainer.appendChild(scoreElement);
